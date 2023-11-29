0e6e28f99b021e9380b15d36a29855af8b54ca18
Dr.M.Sajid <geosajid@gmail.com>
2023-11-29 21:15:25 +0800

    JavaScript Basic Functions

--- bootcamp/06_JavaScript/index.js
+++ bootcamp/06_JavaScript/index.js
@@ -4,8 +4,16 @@ console.log(a);
 console.log(b);
 
 var c = a;
-var a = b;
-var b = c;
+a = b;
+b = c;
 
 console.log(a);
 console.log(b);
+
+function dogAge_to_human(age) {
+  dogAge = age; //console.("What is the dog age?");
+  humanAge = (dogAge - 2) * 4 + 21;
+  console.log("if the doge is " + age + " then its eqvalent to Human Age of " + humanAge);
+}
+
+dogAge_to_human(10);
