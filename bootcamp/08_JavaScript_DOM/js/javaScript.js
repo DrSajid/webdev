c153bcb5752a621ba83da264f7ee7b598c9b5609
Dr.M.Sajid <geosajid@gmail.com>
2023-12-04 20:46:46 +0800

    DOM manipulation through JS

--- bootcamp/08_JavaScript_DOM/js/javaScript.js
+++ bootcamp/08_JavaScript_DOM/js/javaScript.js
@@ -1,1 +1,11 @@
 document.querySelector("h1").innerHTML = "Good Job Sajid";
+
+// document.querySelector("p").classList.remove("font_bigger");
+// document.querySelector("p").classList.add("font_medium");
+document.querySelector("p").classList.add("font_bigger");
+
+document.querySelector("p").innerText = "Updated text";
+
+console.log(document.querySelector("ul a").attributes);
+console.log(document.querySelector("ul a").getAttribute("href"));
+document.querySelector("ul a").setAttribute("href", "https://www.bing.com");
