1acfaba4b991549a3a0b26c599d7e4d65e1836ed
Dr.M.Sajid <geosajid@gmail.com>
2023-12-11 20:45:53 +0800

    event listener

---
+++ bootcamp/10_Drum_kit/index.js
@@ -0,0 +1,29 @@
+let bt_w = document.querySelector("button.w");
+let bt_a = document.querySelector("button.a");
+let bt_s = document.querySelector("button.s");
+let bt_d = document.querySelector("button.d");
+let bt_j = document.querySelector("button.j");
+let bt_k = document.querySelector("button.k");
+let bt_l = document.querySelector("button.l");
+
+bt_w.addEventListener("click", () => {
+  alert("W");
+});
+bt_l.addEventListener("click", () => {
+  alert("L");
+});
+bt_a.addEventListener("click", () => {
+  alert("A");
+});
+bt_s.addEventListener("click", () => {
+  alert("S");
+});
+bt_d.addEventListener("click", () => {
+  alert("D");
+});
+bt_j.addEventListener("click", () => {
+  alert("J");
+});
+bt_k.addEventListener("click", () => {
+  alert("K");
+});
