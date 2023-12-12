86c52da876390f0f477cd3800d36db170fc800d5
Dr.M.Sajid <geosajid@gmail.com>
2023-12-12 20:50:53 +0800

    added sounds

--- bootcamp/10_Drum_kit/index.js
+++ bootcamp/10_Drum_kit/index.js
@@ -7,23 +7,30 @@ let bt_k = document.querySelector("button.k");
 let bt_l = document.querySelector("button.l");
 
 bt_w.addEventListener("click", () => {
-  alert("W");
+  let audio = new Audio("./sounds/crash.mp3");
+  audio.play();
 });
 bt_l.addEventListener("click", () => {
-  alert("L");
+  let audio = new Audio("./sounds/kick-bass.mp3");
+  audio.play();
 });
 bt_a.addEventListener("click", () => {
-  alert("A");
+  let audio = new Audio("./sounds/snare.mp3");
+  audio.play();
 });
 bt_s.addEventListener("click", () => {
-  alert("S");
+  let audio = new Audio("./sounds/tom-1.mp3");
+  audio.play();
 });
 bt_d.addEventListener("click", () => {
-  alert("D");
+  let audio = new Audio("./sounds/tom-2.mp3");
+  audio.play();
 });
 bt_j.addEventListener("click", () => {
-  alert("J");
+  let audio = new Audio("./sounds/tom-3.mp3");
+  audio.play();
 });
 bt_k.addEventListener("click", () => {
-  alert("K");
+  let audio = new Audio("./sounds/tom-4.mp3");
+  audio.play();
 });
