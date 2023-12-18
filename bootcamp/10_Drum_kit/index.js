41d78b5d6399f2a0a7e5b6abf8324387cd51917c
Dr.M.Sajid <geosajid@gmail.com>
2023-12-18 20:14:20 +0800

    key board press

--- bootcamp/10_Drum_kit/index.js
+++ bootcamp/10_Drum_kit/index.js
@@ -34,3 +34,23 @@ bt_k.addEventListener("click", () => {
   let audio = new Audio("./sounds/tom-4.mp3");
   audio.play();
 });
+
+document.addEventListener("keypress", (event) => {
+  let audio = "";
+  if (event.key == "w") {
+    audio = new Audio("./sounds/crash.mp3");
+  } else if (event.key == "a") {
+    audio = new Audio("./sounds/kick-bass.mp3");
+  } else if (event.key == "s") {
+    audio = new Audio("./sounds/snare.mp3");
+  } else if (event.key == "d") {
+    audio = new Audio("./sounds/tom-1.mp3");
+  } else if (event.key == "j") {
+    audio = new Audio("./sounds/tom-2.mp3");
+  } else if (event.key == "k") {
+    audio = new Audio("./sounds/tom-3.mp3");
+  } else if (event.key == "l") {
+    audio = new Audio("./sounds/tom-4.mp3");
+  }
+  audio.play();
+});
