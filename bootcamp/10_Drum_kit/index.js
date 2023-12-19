de96cba181195a496c58c691e756ddb8ce926531
Dr.M.Sajid <geosajid@gmail.com>
2023-12-19 20:31:56 +0800

    switch statement & callbacks

--- bootcamp/10_Drum_kit/index.js
+++ bootcamp/10_Drum_kit/index.js
@@ -35,22 +35,66 @@ bt_k.addEventListener("click", () => {
   audio.play();
 });
 
+// document.addEventListener("keypress", (event) => {
+//   let audio = "";
+//   if (event.key == "w") {
+//     audio = new Audio("./sounds/crash.mp3");
+//   } else if (event.key == "a") {
+//     audio = new Audio("./sounds/kick-bass.mp3");
+//   } else if (event.key == "s") {
+//     audio = new Audio("./sounds/snare.mp3");
+//   } else if (event.key == "d") {
+//     audio = new Audio("./sounds/tom-1.mp3");
+//   } else if (event.key == "j") {
+//     audio = new Audio("./sounds/tom-2.mp3");
+//   } else if (event.key == "k") {
+//     audio = new Audio("./sounds/tom-3.mp3");
+//   } else if (event.key == "l") {
+//     audio = new Audio("./sounds/tom-4.mp3");
+//   }
+//   audio.play();
+// });
+
+function playAudio(audioFile, key) {
+  let audio = new Audio(audioFile);
+  addAnimButton(key);
+  audio.play();
+}
+
 document.addEventListener("keypress", (event) => {
   let audio = "";
-  if (event.key == "w") {
-    audio = new Audio("./sounds/crash.mp3");
-  } else if (event.key == "a") {
-    audio = new Audio("./sounds/kick-bass.mp3");
-  } else if (event.key == "s") {
-    audio = new Audio("./sounds/snare.mp3");
-  } else if (event.key == "d") {
-    audio = new Audio("./sounds/tom-1.mp3");
-  } else if (event.key == "j") {
-    audio = new Audio("./sounds/tom-2.mp3");
-  } else if (event.key == "k") {
-    audio = new Audio("./sounds/tom-3.mp3");
-  } else if (event.key == "l") {
-    audio = new Audio("./sounds/tom-4.mp3");
+  switch (event.key) {
+    case "w":
+      playAudio("./sounds/crash.mp3", event.key);
+      break;
+    case "a":
+      playAudio("./sounds/kick-bass.mp3", event.key);
+      break;
+    case "s":
+      playAudio("./sounds/snare.mp3", event.key);
+      break;
+    case "d":
+      playAudio("./sounds/tom-1.mp3", event.key);
+      break;
+    case "j":
+      playAudio("./sounds/tom-2.mp3", event.key);
+      break;
+    case "k":
+      playAudio("./sounds/tom-3.mp3", event.key);
+      break;
+    case "l":
+      playAudio("./sounds/tom-4.mp3", event.key);
+      break;
+    default:
+      console.log("no audio");
+      break;
   }
-  audio.play();
 });
+
+function addAnimButton(key) {
+  var test = document.querySelector("." + key);
+  test.classList.add("pressed");
+  setTimeout(function (key) {
+    test.classList.remove("pressed");
+  }, 100);
+}
