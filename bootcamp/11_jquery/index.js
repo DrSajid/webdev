f407d9120bc95f9ed68a563f4791329debca5d0d
Dr.M.Sajid <geosajid@gmail.com>
2023-12-21 21:41:57 +0800

    initial commit for jquery

---
+++ bootcamp/11_jquery/index.js
@@ -0,0 +1,12 @@
+// $("h1").css("background-color", "yellow");
+// $("h1").css("background-color", "blue");
+$(document).ready(function () {
+  $("h1").css("background-color", "red");
+  $("h1").css("color", "white");
+  $("h1").css("font-size", "50px");
+  $("h1").css("font-family", "Arial");
+  $("h1").css("text-align", "center");
+  $("h1").css("text-decoration", "underline");
+  $("h1").css("padding", "20px");
+  $("h1").css("margin", "20px");
+});
